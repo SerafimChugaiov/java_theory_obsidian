dc68710c20e1beb8f423c10585ff476c2669dc25
serafym.chuhaiov <serafym.chuhaiov@sonomics.com>
2025-06-26 02:46:30 +0300

    update

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Java Theory",
+    pageTitle: "Java Theory 2",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
